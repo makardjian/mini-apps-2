990b7c0ab24a1e20a0febba541a904310e2871a7
Michael Kardjian <makardjian@gmail.com>
2019-01-31 11:01:04 -0800

    set up file structure, init bundle.js and express server

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,13 @@
+const express = require('express');
+const PORT = 3002;
+const bodyParser = require('body-parser');
+const Path = require('path');
 
+const app = express();
+
+app.use(express.static(Path.join(__dirname, '/public/')));
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+})
 
